4b1ec2450a4a937170c53228df9c63aa19a4d480
Michael Audi <audim@oregonstate.edu>
2025-12-03 23:10:03 -0500

    retry

--- test_client.py
+++ test_client.py
@@ -1,23 +1,11 @@
 import requests
 
-url_num = 'https://cs361-group-41-randomizer-microservice.onrender.com/rand/num'
-url_event = 'https://cs361-group-41-randomizer-microservice.onrender.com/rand/event'
-url_weighted = 'https://cs361-group-41-randomizer-microservice.onrender.com/rand/weighted'
-
-
-def test_num(data):
-    # send JSON data to server
-    response = requests.post(url_num, json = data)
-
-    in_data = response.json()
-
-    if in_data.get('status') == 'success':
-        print(f"Your random number is: {in_data.get('return_value')}")
+url_prompt = 'https://cs361-group-41-image-microservice.onrender.com/image/prompt'
 
 
 def test_event(data):
     # send JSON data to server
-    response = requests.post(url_event, json = data)
+    response = requests.post(url_prompt, json = data)
 
     in_data = response.json()
 
@@ -25,26 +13,5 @@ def test_event(data):
         print(f"Your random event is: {in_data.get('return_value')}")
 
 
-def test_weighted(data):
-    # send JSON data to server
-    response = requests.post(url_weighted, json = data)
-
-    in_data = response.json()
-
-    if in_data.get('status') == 'success':
-        print(f"Your weighted random event is: {in_data.get('return_value')}")
-
-
-start, end = 1, 15
-events = ['event_1', 'event_2', 'event_3', 'event_4']
-weights = [20, 70, 5, 5]
-
-
-print("Random int:")
-test_num({'type': 'int', 'start': start, 'end': end})
-print("Random float")
-test_num({'type': 'float', 'start': start, 'end': end})
-print("Random event:")
-test_event({'events': events})
-print("Weighted random event:")
-test_weighted({'events': events, 'weights': weights})
+image = test_event({'prompt': "Generate a baseball card for a player named Josh on the Cats team."})
+print(image)
